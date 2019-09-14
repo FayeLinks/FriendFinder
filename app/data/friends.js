c6cd30c98c2ce5673e28eaaba7fa947ddf65f578
Alison Kelly <alisonbv@gmail.com>
2019-09-13 20:40:17 -0400

    Added more functionality to check user input vs. hard-coded friends scores, but I'm unable to get the modal to work.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // Starter friend Object
 var friends = [{
     name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo:"https://tse1.mm.bing.net/th?id=OIP._ywYlRUFPRJl-__xT0d1kgHaFj&pid=Api&P=0&w=222&h=168",
     scores:[
         5,
         1,
@@ -13,6 +13,60 @@ var friends = [{
         5,
         4,
         1
+      ]
+  },
+
+
+  {
+    name:"Abbey",
+    photo:"http://images2.fanpop.com/image/photos/9400000/Funny-Cats-cats-9474038-1600-1200.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        2,
+        5,
+        1,
+        4,
+        5,
+        4,
+        1
+      ]
+  },
+
+
+  {
+    name:"Jason",
+    photo:"http://1.bp.blogspot.com/-F9B7MrG9Vl8/T9uevPioqPI/AAAAAAAAACg/FQxPGkupdMk/s1600/cute+cat+pictures+(8).jpg",
+    scores:[
+        5,
+        3,
+        4,
+        3,
+        1,
+        1,
+        4,
+        2,
+        4,
+        1
+      ]
+  },
+
+
+  {
+    name:"Fred",
+    photo:"https://3.bp.blogspot.com/-fGMxWXNU2mM/UiD3xgZTW5I/AAAAAAAAAU0/teZNE2V3qeA/s1600/cat-pictures.jpg",
+    scores:[
+        1,
+        2,
+        4,
+        3,
+        5,
+        2,
+        2,
+        4,
+        4,
+        5
       ]
   }];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,10 +9,40 @@ module.exports = function(app) {
         res.json(friends);
     });
 
+
+
     // Set the post for the api/friends route
     app.post("/api/friends", function(req, res) {
         // This adds the new users sent data object to friends.js
+        // add this line below at the end so the user doesn't become their own best friend
+        for (var i = 0; i < friends.length; i++) {
+            // console.log(req.body.name);
+            // console.log(friends[i].name);
+            for (var j = 0; j < req.body.scores.length; j++) {
+                // parseInt for scores
+                // console.log(parseInt(req.body.scores[j]));
+                // console.log(friends[i].scores[j]);
+            }
+        }
+
+        var bestFriendIndex = 0;
+        var minimumDifference = 40;
+
+        for (var i = 0; i < friends[i].length; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var difference = Math.abs(user.score[j] - friends[i].scores[j]);
+                totalDifference += difference;
+            }
+            if(totalDifference < minimumDifference) {
+                bestFriendIndex = i;
+                minimumDifference = totalDifference;
+            }
+        }
         friends.push(req.body);
+
+        // send back to browser the best friend match
+    res.json(friends[bestFriendIndex]);
     });
 }
 
